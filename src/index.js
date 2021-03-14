143491b9cb53c2c2f1c88b62892ec6145780fbc7
Janemasha <janemasharo@gmail.com>
2021-03-14 18:37:46 +0300

    download solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function reverse (n) {
-  
+    if (n >= 0) {
+    let str = String(n);
+    let mass = str.split('');
+    let mass1 = mass.reverse();
+    let str1 = mass1.join('');
+    let num = Number(str1);
+  return num;
+    }
+    if (n < 0) {
+        let str = String(-n);
+        let mass = str.split('');
+        let mass1 = mass.reverse();
+        let str1 = mass1.join('');
+        let num = Number(str1);
+  return num;
+    }
 }
